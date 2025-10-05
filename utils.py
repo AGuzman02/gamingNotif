7b1b6531eab08dd33d77173baa2f0b490ad71c14
Alex Guzman <95500960+AGuzman02@users.noreply.github.com>
2025-10-05 16:56:00 -0600

    SQL call optimization (#3)
    * small optimization no need to check if the member exists, let upsert fail instead
    
    * addressed comments

--- utils.py
+++ utils.py
@@ -53,15 +53,9 @@ def is_user_leaving_voice(before, after):
 
 async def handleVoiceJoin(member, db: DatabaseQueries):
     """Handle new user by checking and inserting into DB"""
-    if await db.existsMember(member):
-        await db.logArrivalTime(member)
-        return
-    else:
-        if await db.newMember(member):
-            await db.logArrivalTime(member)
-            return
+    await db.newMember(member)
     await db.newMemberToGuild(member, member.guild)
-    return False
+    await db.logArrivalTime(member)
         
 
 async def handleVoiceLeave(member, db: DatabaseQueries):
